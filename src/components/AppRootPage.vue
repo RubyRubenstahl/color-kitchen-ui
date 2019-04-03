0be0bb647bf2aef237edf29948abd16eae39edd9
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-03 16:56:31 -0400

    Implements AppRootPage

---
+++ src/components/AppRootPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-flex>
+        <div class="headline">{{title}}</div>
+        <slot></slot>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+</v-content>
+<script>
+export default {
+  computed: {
+    title: function() {
+      return this.$route.meta.title;
+    }
+  }
+};
+</script>
+
