852fdbd553bcf86d523f44fd1ef82a20b4feaf06
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-03 12:48:19 -0400

    Theme color changes

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 export default {
-    primary: '#1976D2',
+    primary: '#4ab1f0',
     secondary: '#424242',
     accent: '#82B1FF',
-    error: '#FF5252',
+    error: '#f35454',
     info: '#2196F3',
     success: '#4CAF50',
     warning: '#FFC107'
