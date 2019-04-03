4c29be8659bb36ad367aab7dbb46ca85bf11f810
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-03 16:57:23 -0400

    Add users management route & page.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login'
+import Users from './views/Users'
 import Home from './views/Home'
 
 Vue.use(Router)
@@ -22,7 +23,7 @@ export default new Router({
     {
       path: '/users',
       name: 'users',
-      component: Home,
+      component: Users,
       meta: {
         icon: 'group',
         title: 'Manage Users'
---
+++ src/views/Users.vue
@@ -0,0 +1,14 @@
+<template>
+  <app-root-page>
+    <div class="home">Manage, manage, manage</div>
+  </app-root-page>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppRootPage from "../components/AppRootPage";
+export default {
+  name: "home",
+  components: { AppRootPage }
+};
+</script>
