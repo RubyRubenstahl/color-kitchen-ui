48d3c0fdc79fe6e19cf774d25dd8755da25d8a5f
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-03 12:42:18 -0400

    Add feedback to the login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,59 +1,57 @@
 <template>
-  <v-form ref="form" v-model="valid" lazy-validation>
-    <v-text-field
-      autofocus
-      v-model="email"
-      value="ruby"
-      :rules="emailRules"
-      label="Email"
-      prepend-inner-icon="person"
-      style="margin-bottom: 24px"
-      required
-      solo
-      box
-    ></v-text-field>
-
-    <v-text-field
-      v-model="password"
-      :rules="passwordRules"
-      password
-      type="password"
-      solo
-      label="Password"
-      prepend-inner-icon="lock"
-      required
-      box
-      @keydown.enter="login"
-    ></v-text-field>
+  <v-form ref="form" v-model="valid" lazy-validation @input="clearError">
     <v-container>
-      <v-layout row>
-        <v-flex justify-center>
-          <v-btn
-            :disabled="!valid"
-            @click="login"
+      <v-layout column>
+        <v-flex>
+          <v-text-field
+            autofocus
+            v-model="email"
+            value="ruby"
+            :rules="emailRules"
+            label="Email"
+            prepend-inner-icon="person"
+            style="margin-bottom: 24px"
+            required
+            solo
+            box
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            password
+            type="password"
+            solo
+            label="Password"
+            prepend-inner-icon="lock"
+            required
+            box
             @keydown.enter="login"
-            class="justify-right"
-            block
-          >Log In</v-btn>
+          ></v-text-field>
+          <v-sheet
+            transition="scale-transition"
+            v-show="loginError"
+            class="error--text body-2 font-weight-light login-error mt10"
+          >Invalid login</v-sheet>
+          <div class="progress-button">
+            <v-progress-linear v-show="loginPending" :indeterminate="true" height="2" class="ma-0"></v-progress-linear>
+            <v-btn :disabled="!valid" @click="login" @keydown.enter="login" block class="mt-0">
+              <v-icon color="error" transition="scale-transition" v-if="loginError">report_problem</v-icon>&nbsp;&nbsp;
+              <span>Log In</span>
+            </v-btn>
+          </div>
         </v-flex>
       </v-layout>
     </v-container>
-    <!-- 
-    <v-btn color="error" @click="reset">Reset Form</v-btn>
-
-    <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>-->
   </v-form>
 </template>
 
 <script>
 export default {
   data: () => ({
+    errorCleared: true,
     valid: true,
     password: "",
-    passwordRules: [
-      v => !!v || "Password is required",
-      v => (v && v.length >= 8) || "Password must be at least 8 characters"
-    ],
+    passwordRules: [v => !!v || "Password is required"],
     email: "",
     emailRules: [
       v => !!v || "E-mail is required",
@@ -69,12 +67,17 @@ export default {
     },
     reset() {
       this.$refs.form.reset();
+      this.clearError();
     },
     resetValidation() {
       this.$refs.form.resetValidation();
     },
+    clearError() {
+      this.errorCleared = true;
+    },
     login: function(e) {
       console.log(this.password);
+      this.errorCleared = false;
       this.$store.dispatch("auth/authenticate", {
         strategy: "local",
         email: this.email,
@@ -83,6 +86,25 @@ export default {
 
       e.preventDefault();
     }
+  },
+  computed: {
+    loginError: function() {
+      const error = this.$store.state.auth.errorOnAuthenticate;
+      return !this.errorCleared && error ? error : null;
+    },
+    loginPending: function() {
+      console.log(this.$store.state.auth.isAuthenticatePending);
+      return this.$store.state.auth.isAuthenticatePending;
+    }
   }
 };
 </script>
+<style>
+.login-error {
+  width: 100%;
+  text-align: center;
+  background-color: #66000055 !important;
+  margin-bottom: 8px;
+  padding: 8px;
+}
+</style>
