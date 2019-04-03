2c4ea42bc8579435df7c7d6a0c10e2cc790d6ff8
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-03 15:54:16 -0400

    Moves all main view routing config to router.js

--- src/router.js
+++ src/router.js
@@ -5,15 +5,30 @@ import Home from './views/Home'
 
 Vue.use(Router)
 
+export const viewRoutes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    icon: 'home',
+    title: 'Home'
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: Home,
+    icon: 'group',
+    title: 'Manage Users'
+  },
+
+];
+
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
+    ...viewRoutes,
     {
       path: '/login',
       name: 'login',
--- src/views/NavDrawer.vue
+++ src/views/NavDrawer.vue
@@ -3,16 +3,20 @@
     <v-toolbar flat class="transparent">
       <v-list class="pt-0" dense>
         <v-divider></v-divider>
-
-        <navbar-button name="Home" to="/" icon="home"/>
-        <navbar-button name="Manage Users" to="/users" icon="group"/>
+        <navbar-button
+          v-for="view in views"
+          v-bind:key="view.path"
+          :name="view.title"
+          :to="view.path"
+          :icon="view.icon"
+        />
       </v-list>
     </v-toolbar>
   </v-navigation-drawer>
 </template>
 <script>
 import NavbarButton from "../components/NavbarButton";
-
+import { viewRoutes } from "../router";
 export default {
   components: {
     NavbarButton
@@ -21,6 +25,11 @@ export default {
     return {
       mini: true
     };
+  },
+  computed: {
+    views: function() {
+      return viewRoutes;
+    }
   }
 };
 </script>
