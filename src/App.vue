e97e6753c79b230e469ace04b62f9cabc02ae34d
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-03 00:25:18 -0400

    creates login component (not functional)

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,18 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <login-card/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import LoginCard from "./components/LoginCard";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    LoginCard
   },
   mounted() {
     this.$store.dispatch("auth/authenticate").catch(error => {
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,8 +1,71 @@
 <template>
-    
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      autofocus
+      v-model="email"
+      :rules="emailRules"
+      label="Email"
+      prepend-inner-icon="person"
+      style="margin-bottom: 24px"
+      required
+      solo
+      box
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      password
+      type="password"
+      solo
+      label="Password"
+      prepend-inner-icon="lock"
+      style="margin-bottom: 24px"
+      required
+      box
+    ></v-text-field>
+    <v-container>
+      <v-layout row>
+        <v-flex justify-center>
+          <v-btn :disabled="!valid" @click="validate" class="justify-right" block>Log In</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <!-- 
+    <v-btn color="error" @click="reset">Reset Form</v-btn>
+
+    <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>-->
+  </v-form>
 </template>
+
 <script>
 export default {
-    
-}
+  data: () => ({
+    valid: true,
+    password: "",
+    passwordRules: [
+      v => !!v || "Password is required",
+      v => (v && v.length >= 8) || "Password must be at least 8 characters"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ]
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    }
+  }
+};
 </script>
