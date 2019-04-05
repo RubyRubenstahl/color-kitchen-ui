ead4ccd9a42b58c2fc378d957b473d839c698ab3
rubyrubenstahl <ruby@ruby-lighting.com>
2019-04-05 13:23:18 -0400

    Styling Improvements

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,42 +1,40 @@
 <template>
-  <v-sheet color="tertiary">
-    <feathers-vuex-find service="users" :query="{}" watch="query">
-      <v-list slot-scope="{ items: users }" two-line class="transparent">
-        <div v-for="user in users" v-bind:key="user.id">
-          <v-list-tile @click="selectUser(user)" :class="{selected: isSelected(user)}">
-            <v-list-tile-action>
-              <v-icon v-if="isAdmin(user)">vpn_key</v-icon>
-              <v-icon v-else>account_circle</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{user.firstName}} {{user.lastName}}</v-list-tile-title>
-              <v-list-tile-sub-title>{{user.email}}</v-list-tile-sub-title>
-            </v-list-tile-content>
+  <feathers-vuex-find service="users" :query="{}" watch="query">
+    <v-list slot-scope="{ items: users }" two-line class="ma-0 pa-0">
+      <div v-for="user in users" v-bind:key="user.id">
+        <v-list-tile @click="selectUser(user)" :class="{selected: isSelected(user)}">
+          <v-list-tile-action>
+            <v-icon v-if="isAdmin(user)">vpn_key</v-icon>
+            <v-icon v-else>account_circle</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{user.firstName}} {{user.lastName}}</v-list-tile-title>
+            <v-list-tile-sub-title>{{user.email}}</v-list-tile-sub-title>
+          </v-list-tile-content>
 
-            <v-list-tile-action>
-              <v-menu close-on-content-click close-on-click offset-y>
-                <template v-slot:activator="{ on }">
-                  <v-btn icon v-on="on">
-                    <v-icon>more_vert</v-icon>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-tile>
-                    <v-list-tile-action>
-                      <v-icon>rotate_right</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-title>Reset Password</v-list-tile-title>
-                  </v-list-tile>
-                  <delete-user-action :user="user"></delete-user-action>
-                </v-list>
-              </v-menu>
-            </v-list-tile-action>
-          </v-list-tile>
-          <v-divider/>
-        </div>
-      </v-list>
-    </feathers-vuex-find>
-  </v-sheet>
+          <v-list-tile-action>
+            <v-menu close-on-content-click close-on-click offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon>more_vert</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-action>
+                    <v-icon>rotate_right</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-title>Reset Password</v-list-tile-title>
+                </v-list-tile>
+                <delete-user-action :user="user"></delete-user-action>
+              </v-list>
+            </v-menu>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-divider/>
+      </div>
+    </v-list>
+  </feathers-vuex-find>
 </template>
 <script>
 import DeleteUserAction from "./DeleteUserAction";
@@ -67,6 +65,6 @@ export default {
 </script>
 <style lang="stylus" scoped>
 .selected {
-  background-color: #424242;
+  background-color: #6e6e6e;
 }
 </style>
